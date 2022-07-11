806a43396b26be94ee2b6b3e88d9e40956e2cee4
arjunlaxane <arjunlaxane@gmail.com>
2022-07-11 19:18:23 +0530

    health experts search bar

---
+++ script.js
@@ -0,0 +1,807 @@
+let healthExperts = [
+  {
+    dezignation: 'Doctor',
+    first_name: 'Suresh',
+    last_name: 'Kumar',
+    email: 'suresh@gmail.com',
+    gender: 'Male',
+    specialization: 'General physician',
+    qualification: 'MBBS Medicine',
+    experience: '4 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Harmanprit',
+    last_name: 'Kaur',
+    email: 'harman@gmail.com',
+    gender: 'Female',
+    specialization: 'General physician',
+    qualification: 'MBBS Medicine',
+    experience: '6 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Raju',
+    last_name: 'Seth',
+    email: 'raju@gmail.com',
+    gender: 'Male',
+    specialization: 'Gynecologist',
+    qualification: 'MD Gynecology',
+    experience: '2 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Priyal',
+    last_name: 'Pofali',
+    email: 'priyal@gmail.com',
+    gender: 'Female',
+    specialization: 'Gynecologist',
+    qualification: 'MD Gynecology',
+    experience: '2 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Gopal',
+    last_name: 'Kishan',
+    email: 'gopal@gmail.com',
+    gender: 'Male',
+    specialization: 'Pediatrician',
+    qualification: 'DM Pediatrician',
+    experience: '4 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Anushka',
+    last_name: 'Sharma',
+    email: 'anushka@gmail.com',
+    gender: 'Female',
+    specialization: 'Pediatrician',
+    qualification: 'DM Pediatrician',
+
+    experience: '5 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Dipika',
+    last_name: 'Padukon',
+    email: 'dipika@gmail.com',
+    gender: 'Female',
+    specialization: 'Pediatrician',
+    qualification: 'DM Pediatrician',
+    experience: '6 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Kaustubh',
+    last_name: 'Kumar',
+    email: 'kaustubh@gmail.com',
+    gender: 'Male',
+    specialization: 'Ophthalmologist',
+    qualification: 'MD Ophthalmology',
+    experience: '5 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Kavya',
+    last_name: 'iyer',
+    email: 'kavya@gmail.com',
+    gender: 'Female',
+    specialization: 'Ophthalmologist',
+    qualification: 'MD Ophthalmology',
+    experience: '5 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Rahul',
+    last_name: 'Pawar',
+    email: 'rahul@gmail.com',
+    gender: 'Male',
+    specialization: 'Dermatologist',
+    qualification: 'MD Dermatology',
+    experience: '4 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Maduri',
+    last_name: 'Chaudhari',
+    email: 'madhuri@gmail.com',
+    gender: 'Female',
+    specialization: 'Dermatologist',
+    qualification: 'DM Dermatology',
+    experience: '17 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Tanmay',
+    last_name: 'Laxane',
+    email: 'tanmay@gmail.com',
+    gender: 'Male',
+    specialization: 'Gastroenterologist',
+    qualification: 'DM Gastroenterology',
+    experience: '6 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Ashmika',
+    last_name: 'Jain',
+    email: 'ashmika@gmail.com',
+    gender: 'Female',
+    specialization: 'Gastroenterologist',
+    qualification: 'DM Gastroenterology',
+    experience: '4 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Laxman',
+    last_name: 'Ganesh',
+    email: 'laxman@gmail.com',
+    gender: 'Male',
+    specialization: 'ENT Specialist/ Otolaryngologist',
+    qualification: 'MBBS Otolaryngology',
+    experience: '6 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Karuna',
+    last_name: 'Bisen',
+    email: 'karuna@gmail.com',
+    gender: 'Female',
+    specialization: 'ENT Specialist/ Otolaryngologist',
+    qualification: 'MD Otolaryngology',
+    experience: '16 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Dipak',
+    last_name: 'Matre',
+    email: 'dipak@gmail.com',
+    gender: 'Male',
+    specialization: 'Cardiologist',
+    qualification: 'MD Cardiology',
+    experience: '16 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Varsha',
+    last_name: 'Kaur',
+    email: 'Varsha@gmail.com',
+    gender: 'Female',
+    specialization: 'Cardiologist',
+    qualification: 'MD Cardiology',
+    experience: '14 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Dipak',
+    last_name: 'Matre',
+    email: 'dipak@gmail.com',
+    gender: 'Male',
+    specialization: 'Cardiologist',
+    qualification: 'MD Cardiology',
+    experience: '16 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Amar',
+    last_name: 'Neurgaokar',
+    email: 'amar@gmail.com',
+    gender: 'Male',
+    specialization: 'Orthopedic',
+    qualification: 'MD Orthopedic',
+    experience: '20 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Shabnam',
+    last_name: 'Khan',
+    email: 'shabnam@gmail.com',
+    gender: 'Female',
+    specialization: 'Orthopedic',
+    qualification: 'MD Orthopedic',
+    experience: '21 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Naveen',
+    last_name: 'Shukla',
+    email: 'naveen@gmail.com',
+    gender: 'Male',
+    specialization: 'Neurologist',
+    qualification: 'MD Neurology',
+    experience: '6 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Yashswita',
+    last_name: 'Mohurle',
+    email: 'yashswita@gmail.com',
+    gender: 'Female',
+    specialization: 'Neurologist',
+    qualification: 'MD Neurology',
+    experience: '8 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Vaibhav',
+    last_name: 'Gadle',
+    email: 'vaibhav@gmail.com',
+    gender: 'Male',
+    specialization: 'Dentist',
+    qualification: 'MD Dentist',
+    experience: '3 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Prajkta',
+    last_name: 'Patil',
+    email: 'praju@gmail.com',
+    gender: 'Female',
+    specialization: 'Dentist',
+    qualification: 'MD Dentist',
+    experience: '2 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Varun',
+    last_name: 'Bisen',
+    email: 'varun@gmail.com',
+    gender: 'Male',
+    specialization: 'Urologist',
+    qualification: 'DM Urology',
+    experience: '14 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Anagha',
+    last_name: 'Gadle',
+    email: 'anagha@gmail.com',
+    gender: 'Female',
+    specialization: 'Urologist',
+    qualification: 'DM Urology',
+    experience: '16 Years',
+  },
+  {
+    dezignation: 'Doctor',
+    first_name: 'Kaveri',
+    last_name: 'Sidhhu',
+    email: 'kaveri@gmail.com',
+    gender: 'Female',
+    specialization: 'Psychiatric',
+    qualification: 'MD Psychiatric',
+    experience: '6 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Ankit',
+    last_name: 'Walde',
+    email: 'ankit@gmail.com',
+    gender: 'Male',
+    specialization: 'Psychiatric',
+    qualification: 'DM Psychiatric',
+    experience: '8 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Ankita',
+    last_name: 'Chimote',
+    email: 'ankita@gmail.com',
+    gender: 'Female',
+    specialization: 'Diabetologist',
+    qualification: 'DM Diabetology',
+    experience: '8 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Vanaram',
+    last_name: 'Reddy',
+    email: 'vanaram@gmail.com',
+    gender: 'Male',
+    specialization: 'Diabetologist',
+    qualification: 'DM Diabetology',
+    experience: '18 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Sampada',
+    last_name: 'Dhawande',
+    email: 'sampada@gmail.com',
+    gender: 'Female',
+    specialization: 'Surgeon',
+    qualification: 'DM Surgery',
+    experience: '8 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Anandan',
+    last_name: 'Guvi',
+    email: 'guvi@gmail.com',
+    gender: 'Male',
+    specialization: 'Surgeon',
+    qualification: 'DM Surgery',
+    experience: '12 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Aishwarya',
+    last_name: 'Deshmukh',
+    email: 'aishwarya@gmail.com',
+    gender: 'Female',
+    specialization: 'Oncologist',
+    qualification: 'DM Oncology',
+    experience: '9 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Aukshawant',
+    last_name: 'Rahate',
+    email: 'aukshwant@gmail.com',
+    gender: 'Male',
+    specialization: 'Oncologist',
+    qualification: 'DM Oncology',
+    experience: '19 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Himangi',
+    last_name: 'Deshmukh',
+    email: 'himangi@gmail.com',
+    gender: 'Female',
+    specialization: 'Endocronologist',
+    qualification: 'DM Endocrinology',
+    experience: '9 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Wasim',
+    last_name: 'Khan',
+    email: 'wasim@gmail.com',
+    gender: 'Male',
+    specialization: 'Endocronologist',
+    qualification: 'DM Endocrinology',
+    experience: '19 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Arzu',
+    last_name: 'Sheikh',
+    email: 'arzu@gmail.com',
+    gender: 'Female',
+    specialization: 'Physiotherapist',
+    qualification: 'DM Physiotherapy',
+    experience: '9 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Saurabh',
+    last_name: 'Nikhade',
+    email: 'saurabh@gmail.com',
+    gender: 'Male',
+    specialization: 'Physiotherapist',
+    qualification: 'DM Physiotherapy',
+    experience: '19 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Nargiz',
+    last_name: 'Fakhri',
+    email: 'nargiz@gmail.com',
+    gender: 'Female',
+    specialization: 'Homeopathist',
+    qualification: 'Homeopathy',
+    experience: '5 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Karan',
+    last_name: 'Chhabra',
+    email: 'karan@gmail.com',
+    gender: 'Male',
+    specialization: 'Homeopathist',
+    qualification: ' Homeopathy',
+    experience: '15 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Sanjay',
+    last_name: 'Kolhe',
+    email: 'sanjay@gmail.com',
+    gender: 'Male',
+    specialization: 'Ayurvedic',
+    qualification: 'Ayurveda',
+    experience: '5 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Reshma',
+    last_name: 'Kochin',
+    email: 'reshma@gmail.com',
+    gender: 'Female',
+    specialization: 'Ayurvedic',
+    qualification: 'Ayurveda',
+    experience: '1  5 Years',
+  },
+
+  {
+    dezignation: 'Yoga Expert',
+    first_name: 'Radhe',
+    last_name: 'Bhai',
+    email: 'radhe@gmail.com',
+    gender: 'Male',
+    specialization: 'Yoga',
+    qualification: 'Diploma in Yoga',
+    experience: '14 Years',
+  },
+
+  {
+    dezignation: 'Yoga Expert',
+    first_name: 'Radhika',
+    last_name: 'Menon',
+    email: 'radhika@gmail.com',
+    gender: 'Female',
+    specialization: 'Yoga',
+    qualification: 'Diploma in Yoga',
+    experience: '19 Years',
+  },
+
+  {
+    dezignation: 'Yoga Expert',
+    first_name: 'Yashoda',
+    last_name: 'Mai',
+    email: 'yashoda@gmail.com',
+    gender: 'Female',
+    specialization: 'Yoga',
+    qualification: 'Diploma in Yoga',
+    experience: '2 Years',
+  },
+
+  {
+    dezignation: 'Yoga Expert',
+    first_name: 'Yusuf',
+    last_name: 'Pappuwale',
+    email: 'yusuf@gmail.com',
+    gender: 'Male',
+    specialization: 'Yoga',
+    qualification: 'Diploma in Yoga',
+    experience: '10 Years',
+  },
+
+  {
+    dezignation: 'Yoga Expert',
+    first_name: 'George',
+    last_name: 'Lever',
+    email: 'george@gmail.com',
+    gender: 'Male',
+    specialization: 'Yoga',
+    qualification: 'Diploma in Yoga',
+    experience: '12 Years',
+  },
+
+  {
+    dezignation: 'Yoga Expert',
+    first_name: 'Rano',
+    last_name: 'Kumari',
+    email: 'rano@gmail.com',
+    gender: 'Female',
+    specialization: 'Yoga',
+    qualification: 'Diploma in Yoga',
+    experience: '14 Years',
+  },
+
+  {
+    dezignation: 'Yoga Expert',
+    first_name: 'Diggi',
+    last_name: 'Raja',
+    email: 'diggi@gmail.com',
+    gender: 'Male',
+    specialization: 'Yoga',
+    qualification: 'Diploma in Yoga',
+    experience: '14 Years',
+  },
+
+  {
+    dezignation: 'Yoga Expert',
+    first_name: 'Barak',
+    last_name: 'Obama',
+    email: 'barak@gmail.com',
+    gender: 'Male',
+    specialization: 'Yoga',
+    qualification: 'Diploma in Yoga',
+    experience: '16 Years',
+  },
+
+  {
+    dezignation: 'Yoga Expert',
+    first_name: 'Mischelle',
+    last_name: 'Depp',
+    email: 'depp@gmail.com',
+    gender: 'Female',
+    specialization: 'Yoga',
+    qualification: 'Diploma in Yoga',
+    experience: '19 Years',
+  },
+
+  {
+    dezignation: 'Nutritionist',
+    first_name: 'Radha',
+    last_name: 'Sontakke',
+    email: 'radha@gmail.com',
+    gender: 'Female',
+    specialization: 'Nutrition/Dietician',
+    qualification: 'MBBS',
+    experience: '4 Years',
+  },
+
+  {
+    dezignation: 'Nutritionist',
+    first_name: 'Imran',
+    last_name: 'Khan',
+    email: 'imran@gmail.com',
+    gender: 'Male',
+    specialization: 'Nutrition/Dietician',
+    qualification: 'MBBS',
+    experience: '14 Years',
+  },
+
+  {
+    dezignation: 'Nutritionist',
+    first_name: 'Mahendra',
+    last_name: 'Singh',
+    email: 'singh@gmail.com',
+    gender: 'Male',
+    specialization: 'Nutrition/Dietician',
+    qualification: 'MBBS',
+    experience: '14 Years',
+  },
+
+  {
+    dezignation: 'Nutritionist',
+    first_name: 'Anjelina',
+    last_name: 'Julie',
+    email: 'angelina@gmail.com',
+    gender: 'Female',
+    specialization: 'Nutrition/Dietician',
+    qualification: 'MD',
+    experience: '12 Years',
+  },
+
+  {
+    dezignation: 'Nutritionist',
+    first_name: 'Brad',
+    last_name: 'Pitt',
+    email: 'pitt@gmail.com',
+    gender: 'Male',
+    specialization: 'Nutrition/Dietician',
+    qualification: 'MBBS',
+    experience: '8 Years',
+  },
+
+  {
+    dezignation: 'Nutritionist',
+    first_name: 'Brendom',
+    last_name: 'Macculum',
+    email: 'bred@gmail.com',
+    gender: 'Female',
+    specialization: 'Nutrition/Dietician',
+    qualification: 'MBBS',
+    experience: '5 Years',
+  },
+
+  {
+    dezignation: 'Zumba Instructor',
+    first_name: 'Bobby',
+    last_name: 'Parker',
+    email: 'bobby@gmail.com',
+    gender: 'Male',
+    specialization: 'Fitness',
+    qualification: 'Zumba Course',
+    experience: '6 Years',
+  },
+
+  {
+    dezignation: 'Zumba Instructor',
+    first_name: 'Sunny',
+    last_name: 'Deol',
+    email: 'sunny@gmail.com',
+    gender: 'Male',
+    specialization: 'Fitness',
+    qualification: 'Zumba Course',
+    experience: '6 Years',
+  },
+
+  {
+    dezignation: 'Zumba Instructor',
+    first_name: 'Kareena',
+    last_name: 'Khan',
+    email: 'kareena@gmail.com',
+    gender: 'Female',
+    specialization: 'Fitness',
+    qualification: 'Zumba Course',
+    experience: '6 Years',
+  },
+
+  {
+    dezignation: 'Zumba Instructor',
+    first_name: 'Bipasha',
+    last_name: 'Dhonde',
+    email: 'dhonde@gmail.com',
+    gender: 'Female',
+    specialization: 'Fitness',
+    qualification: 'Zumba Course',
+    experience: '16 Years',
+  },
+
+  {
+    dezignation: 'Zumba Instructor',
+    first_name: 'Shahrukh',
+    last_name: 'Sheikh',
+    email: 'Sheikh123@gmail.com',
+    gender: 'Male',
+    specialization: 'Fitness',
+    qualification: 'Zumba Course',
+    experience: '9 Years',
+  },
+
+  {
+    dezignation: 'Zumba Instructor',
+    first_name: 'Mohamed',
+    last_name: 'Khan',
+    email: 'khan1236@gmail.com',
+    gender: 'Male',
+    specialization: 'Fitness',
+    qualification: 'Zumba Course',
+    experience: '9 Years',
+  },
+
+  {
+    dezignation: 'Zumba Instructor',
+    first_name: 'Ajay',
+    last_name: 'Devgan',
+    email: 'ajay@gmail.com',
+    gender: 'Male',
+    specialization: 'Fitness',
+    qualification: 'Zumba Course',
+    experience: '6 Years',
+  },
+
+  {
+    dezignation: 'Gym Instructor',
+    first_name: 'James',
+    last_name: 'Jackson',
+    email: 'james@gmail.com',
+    gender: 'Male',
+    specialization: 'Fitness',
+    qualification: 'Gym Course',
+    experience: '6 Years',
+  },
+
+  {
+    dezignation: 'Doctor',
+    first_name: 'Sriram',
+    last_name: 'Thakur',
+    email: 'sriram@gmail.com',
+    gender: 'Male',
+    specialization: '',
+    qualification: 'MD',
+    experience: '6 Years',
+  },
+
+  {
+    dezignation: 'Gym Instructor',
+    first_name: 'Michael',
+    last_name: 'Jackson',
+    email: 'mcjks@gmail.com',
+    gender: 'Male',
+    specialization: 'Fitness',
+    qualification: 'Gym Course',
+    experience: '8 Years',
+  },
+
+  {
+    dezignation: 'Gym Instructor',
+    first_name: 'Pranav',
+    last_name: 'Gawande',
+    email: 'pranav@gmail.com',
+    gender: 'Male',
+    specialization: 'Fitness',
+    qualification: 'Gym Course',
+    experience: '9 Years',
+  },
+
+  {
+    dezignation: 'Gym Instructor',
+    first_name: 'Shalini',
+    last_name: 'Mohan',
+    email: 'shalini@gmail.com',
+    gender: 'Female',
+    specialization: 'Fitness',
+    qualification: 'Gym Course',
+    experience: '11 Years',
+  },
+];
+
+let searchBox = document.querySelector('#search-box');
+//use keyup or keydown event-keyboard events
+searchBox.addEventListener('keyup', function () {
+  let textEntered = searchBox.value; //user entered text
+  //console.log(textEntered);
+
+  let filteredExperts = [];
+  if (textEntered != '') {
+    //populate data on search box
+
+    filteredExperts = healthExperts.filter(function (healthExpert) {
+      return (
+        healthExpert.first_name
+          .toLowerCase()
+          .includes(textEntered.toLowerCase()) ||
+        healthExpert.last_name
+          .toLowerCase()
+          .includes(textEntered.toLowerCase()) ||
+        healthExpert.gender.toLowerCase() == textEntered.toLowerCase() ||
+        healthExpert.dezignation
+          .toLowerCase()
+          .includes(textEntered.toLowerCase()) ||
+        healthExpert.specialization
+          .toLowerCase()
+          .includes(textEntered.toLowerCase()) ||
+        healthExpert.qualification
+          .toLowerCase()
+          .includes(textEntered.toLowerCase()) ||
+        healthExpert.experience
+          .toLowerCase()
+          .includes(textEntered.toLowerCase())
+      );
+    });
+    displayEmployess(filteredExperts);
+  }
+});
+
+//create Display data function to get the data if user type
+let displayEmployess = healthExperts => {
+  //select the table
+  let tableBody = document.querySelector('#table-body');
+
+  //Initially the data in table will be empty so, clear the table body
+  let tableRows = ''; //it will keep row empty before new data to display after clicking button otherwise it will display 1 data over another.
+  for (let healthExpert of healthExperts) {
+    tableRows += `
+      <tr>
+      <td>${healthExpert.dezignation}</td>
+      <td>${healthExpert.first_name}</td>
+      <td>${healthExpert.last_name}</td>
+      <td>${healthExpert.email}</td>
+      <td>${healthExpert.gender}</td>
+      <td>${healthExpert.specialization}</td>
+      <td>${healthExpert.qualification}</td>
+      <td>${healthExpert.experience}</td>
+
+      </tr>
+      `;
+    tableBody.innerHTML = tableRows;
+    // tableBody.append(tableRows);
+  }
+};
